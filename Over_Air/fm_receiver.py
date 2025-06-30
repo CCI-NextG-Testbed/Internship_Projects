10681224224d67f406ea5b2719b15ea94211b2f3
Kurokku <Kurokku@vt.edu>
2025-06-30 14:59:04 -0400

    changes

--- Over_Air/fm_receiver.py
+++ Over_Air/fm_receiver.py
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: GPL-3.0
 #
 # GNU Radio Python Flow Graph
-# Title: USRP Spectral Analysis
+# Title: FM Receiver
 # Author: John Sylvia
 # GNU Radio version: 3.10.9.2
 
@@ -13,7 +13,7 @@ from PyQt5 import Qt
 from gnuradio import qtgui
 from PyQt5 import QtCore
 from gnuradio import analog
-from gnuradio import blocks
+from gnuradio import audio
 from gnuradio import filter
 from gnuradio.filter import firdes
 from gnuradio import gr
@@ -24,16 +24,18 @@ from PyQt5 import Qt
 from argparse import ArgumentParser
 from gnuradio.eng_arg import eng_float, intx
 from gnuradio import eng_notation
+from gnuradio import uhd
+import time
 import sip
 
 
 
-class usrp_sprectral_analysis(gr.top_block, Qt.QWidget):
+class fm_receiver(gr.top_block, Qt.QWidget):
 
     def __init__(self):
-        gr.top_block.__init__(self, "USRP Spectral Analysis", catch_exceptions=True)
+        gr.top_block.__init__(self, "FM Receiver", catch_exceptions=True)
         Qt.QWidget.__init__(self)
-        self.setWindowTitle("USRP Spectral Analysis")
+        self.setWindowTitle("FM Receiver")
         qtgui.util.check_set_qss()
         try:
             self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
@@ -51,7 +53,7 @@ class usrp_sprectral_analysis(gr.top_block, Qt.QWidget):
         self.top_grid_layout = Qt.QGridLayout()
         self.top_layout.addLayout(self.top_grid_layout)
 
-        self.settings = Qt.QSettings("GNU Radio", "usrp_sprectral_analysis")
+        self.settings = Qt.QSettings("GNU Radio", "fm_receiver")
 
         try:
             geometry = self.settings.value("geometry")
@@ -63,16 +65,36 @@ class usrp_sprectral_analysis(gr.top_block, Qt.QWidget):
         ##################################################
         # Variables
         ##################################################
-        self.samp_rate = samp_rate = 32000
-        self.gain = gain = 0
-        self.center_freq = center_freq = 1000
-        self.bandwidth = bandwidth = 100
-        self.variable_band_pass_filter_taps_0 = variable_band_pass_filter_taps_0 = firdes.band_pass(10**(gain/10), samp_rate, center_freq-bandwidth/2, center_freq+bandwidth/2, bandwidth/4, window.WIN_HAMMING, 6.76)
+        self.samp_rate = samp_rate = 2*10**6
+        self.freq_2 = freq_2 = 0
+        self.freq = freq = 87.9*10**6
 
         ##################################################
         # Blocks
         ##################################################
 
+        self._freq_range = qtgui.Range(87.9*10**6, 107.9*10**6, 200000, 87.9*10**6, 200)
+        self._freq_win = qtgui.RangeWidget(self._freq_range, self.set_freq, "Frequency", "counter_slider", float, QtCore.Qt.Horizontal)
+        self.top_layout.addWidget(self._freq_win)
+        self.uhd_usrp_source_0 = uhd.usrp_source(
+            ",".join(("", '')),
+            uhd.stream_args(
+                cpu_format="fc32",
+                args='',
+                channels=list(range(0,1)),
+            ),
+        )
+        self.uhd_usrp_source_0.set_samp_rate(samp_rate)
+        self.uhd_usrp_source_0.set_time_unknown_pps(uhd.time_spec(0))
+
+        self.uhd_usrp_source_0.set_center_freq(freq, 0)
+        self.uhd_usrp_source_0.set_antenna("RX2", 0)
+        self.uhd_usrp_source_0.set_rx_agc(True, 0)
+        self.rational_resampler_xxx_0 = filter.rational_resampler_ccc(
+                interpolation=12,
+                decimation=125,
+                taps=[],
+                fractional_bw=0)
         self.qtgui_time_sink_x_0 = qtgui.time_sink_c(
             1024, #size
             samp_rate, #samp_rate
@@ -126,8 +148,8 @@ class usrp_sprectral_analysis(gr.top_block, Qt.QWidget):
         self.top_layout.addWidget(self._qtgui_time_sink_x_0_win)
         self.qtgui_freq_sink_x_0 = qtgui.freq_sink_c(
             1024, #size
-            window.WIN_HAMMING, #wintype
-            0, #fc
+            window.WIN_BLACKMAN_hARRIS, #wintype
+            freq, #fc
             samp_rate, #bw
             "", #name
             1,
@@ -138,7 +160,7 @@ class usrp_sprectral_analysis(gr.top_block, Qt.QWidget):
         self.qtgui_freq_sink_x_0.set_y_label('Relative Gain', 'dB')
         self.qtgui_freq_sink_x_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, 0.0, 0, "")
         self.qtgui_freq_sink_x_0.enable_autoscale(False)
-        self.qtgui_freq_sink_x_0.enable_grid(True)
+        self.qtgui_freq_sink_x_0.enable_grid(False)
         self.qtgui_freq_sink_x_0.set_fft_average(1.0)
         self.qtgui_freq_sink_x_0.enable_axis_labels(True)
         self.qtgui_freq_sink_x_0.enable_control_panel(False)
@@ -166,43 +188,28 @@ class usrp_sprectral_analysis(gr.top_block, Qt.QWidget):
 
         self._qtgui_freq_sink_x_0_win = sip.wrapinstance(self.qtgui_freq_sink_x_0.qwidget(), Qt.QWidget)
         self.top_layout.addWidget(self._qtgui_freq_sink_x_0_win)
-        self._gain_range = qtgui.Range(0, 20, 1, 0, 200)
-        self._gain_win = qtgui.RangeWidget(self._gain_range, self.set_gain, "Gain (dB)", "counter_slider", float, QtCore.Qt.Horizontal)
-        self.top_grid_layout.addWidget(self._gain_win, 0, 0, 1, 1)
-        for r in range(0, 1):
-            self.top_grid_layout.setRowStretch(r, 1)
-        for c in range(0, 1):
-            self.top_grid_layout.setColumnStretch(c, 1)
-        self.freq_xlating_fir_filter_xxx_0 = filter.freq_xlating_fir_filter_ccc(1, variable_band_pass_filter_taps_0, 0, samp_rate)
-        self._center_freq_range = qtgui.Range(0, 20000, 1, 1000, 200)
-        self._center_freq_win = qtgui.RangeWidget(self._center_freq_range, self.set_center_freq, "Center Frequency", "counter_slider", float, QtCore.Qt.Horizontal)
-        self.top_grid_layout.addWidget(self._center_freq_win, 1, 0, 1, 1)
-        for r in range(1, 2):
-            self.top_grid_layout.setRowStretch(r, 1)
-        for c in range(0, 1):
-            self.top_grid_layout.setColumnStretch(c, 1)
-        self.blocks_throttle2_0 = blocks.throttle( gr.sizeof_gr_complex*1, samp_rate, True, 0 if "auto" == "auto" else max( int(float(0.1) * samp_rate) if "auto" == "time" else int(0.1), 1) )
-        self._bandwidth_range = qtgui.Range(0, 1000, 1, 100, 200)
-        self._bandwidth_win = qtgui.RangeWidget(self._bandwidth_range, self.set_bandwidth, "Bandwidth", "counter_slider", float, QtCore.Qt.Horizontal)
-        self.top_grid_layout.addWidget(self._bandwidth_win, 2, 0, 1, 1)
-        for r in range(2, 3):
-            self.top_grid_layout.setRowStretch(r, 1)
-        for c in range(0, 1):
-            self.top_grid_layout.setColumnStretch(c, 1)
-        self.analog_noise_source_x_0 = analog.noise_source_c(analog.GR_GAUSSIAN, 1, 0)
+        self._freq_2_range = qtgui.Range(0, 10000, 1, 0, 200)
+        self._freq_2_win = qtgui.RangeWidget(self._freq_2_range, self.set_freq_2, "Frequency2", "counter_slider", float, QtCore.Qt.Horizontal)
+        self.top_layout.addWidget(self._freq_2_win)
+        self.audio_sink_0 = audio.sink(48000, '', True)
+        self.analog_wfm_rcv_0 = analog.wfm_rcv(
+        	quad_rate=(48000*4),
+        	audio_decimation=4,
+        )
 
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.analog_noise_source_x_0, 0), (self.freq_xlating_fir_filter_xxx_0, 0))
-        self.connect((self.blocks_throttle2_0, 0), (self.qtgui_freq_sink_x_0, 0))
-        self.connect((self.blocks_throttle2_0, 0), (self.qtgui_time_sink_x_0, 0))
-        self.connect((self.freq_xlating_fir_filter_xxx_0, 0), (self.blocks_throttle2_0, 0))
+        self.connect((self.analog_wfm_rcv_0, 0), (self.audio_sink_0, 0))
+        self.connect((self.rational_resampler_xxx_0, 0), (self.analog_wfm_rcv_0, 0))
+        self.connect((self.uhd_usrp_source_0, 0), (self.qtgui_freq_sink_x_0, 0))
+        self.connect((self.uhd_usrp_source_0, 0), (self.qtgui_time_sink_x_0, 0))
+        self.connect((self.uhd_usrp_source_0, 0), (self.rational_resampler_xxx_0, 0))
 
 
     def closeEvent(self, event):
-        self.settings = Qt.QSettings("GNU Radio", "usrp_sprectral_analysis")
+        self.settings = Qt.QSettings("GNU Radio", "fm_receiver")
         self.settings.setValue("geometry", self.saveGeometry())
         self.stop()
         self.wait()
@@ -214,43 +221,28 @@ class usrp_sprectral_analysis(gr.top_block, Qt.QWidget):
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
-        self.set_variable_band_pass_filter_taps_0(firdes.band_pass(10**(self.gain/10), self.samp_rate, self.center_freq-self.bandwidth/2, self.center_freq+self.bandwidth/2, self.bandwidth/4, window.WIN_HAMMING, 6.76))
-        self.blocks_throttle2_0.set_sample_rate(self.samp_rate)
-        self.qtgui_freq_sink_x_0.set_frequency_range(0, self.samp_rate)
+        self.qtgui_freq_sink_x_0.set_frequency_range(self.freq, self.samp_rate)
         self.qtgui_time_sink_x_0.set_samp_rate(self.samp_rate)
+        self.uhd_usrp_source_0.set_samp_rate(self.samp_rate)
 
-    def get_gain(self):
-        return self.gain
+    def get_freq_2(self):
+        return self.freq_2
 
-    def set_gain(self, gain):
-        self.gain = gain
-        self.set_variable_band_pass_filter_taps_0(firdes.band_pass(10**(self.gain/10), self.samp_rate, self.center_freq-self.bandwidth/2, self.center_freq+self.bandwidth/2, self.bandwidth/4, window.WIN_HAMMING, 6.76))
+    def set_freq_2(self, freq_2):
+        self.freq_2 = freq_2
 
-    def get_center_freq(self):
-        return self.center_freq
+    def get_freq(self):
+        return self.freq
 
-    def set_center_freq(self, center_freq):
-        self.center_freq = center_freq
-        self.set_variable_band_pass_filter_taps_0(firdes.band_pass(10**(self.gain/10), self.samp_rate, self.center_freq-self.bandwidth/2, self.center_freq+self.bandwidth/2, self.bandwidth/4, window.WIN_HAMMING, 6.76))
-
-    def get_bandwidth(self):
-        return self.bandwidth
-
-    def set_bandwidth(self, bandwidth):
-        self.bandwidth = bandwidth
-        self.set_variable_band_pass_filter_taps_0(firdes.band_pass(10**(self.gain/10), self.samp_rate, self.center_freq-self.bandwidth/2, self.center_freq+self.bandwidth/2, self.bandwidth/4, window.WIN_HAMMING, 6.76))
-
-    def get_variable_band_pass_filter_taps_0(self):
-        return self.variable_band_pass_filter_taps_0
-
-    def set_variable_band_pass_filter_taps_0(self, variable_band_pass_filter_taps_0):
-        self.variable_band_pass_filter_taps_0 = variable_band_pass_filter_taps_0
-        self.freq_xlating_fir_filter_xxx_0.set_taps(self.variable_band_pass_filter_taps_0)
+    def set_freq(self, freq):
+        self.freq = freq
+        self.qtgui_freq_sink_x_0.set_frequency_range(self.freq, self.samp_rate)
+        self.uhd_usrp_source_0.set_center_freq(self.freq, 0)
 
 
 
 
-def main(top_block_cls=usrp_sprectral_analysis, options=None):
+def main(top_block_cls=fm_receiver, options=None):
 
     qapp = Qt.QApplication(sys.argv)
 
